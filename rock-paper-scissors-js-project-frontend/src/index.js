66fa7a33e90028be8826372c6959add293d583a9
Roberto-Livi <robertomlivi@outlook.com>
2020-07-23 15:19:20 -0400

    adding html and css to page

--- rock-paper-scissors-js-project-frontend/src/index.js
+++ rock-paper-scissors-js-project-frontend/src/index.js
@@ -4,3 +4,9 @@ const BACKEND_URL = 'http://localhost:3000';
 fetch(`${BACKEND_URL}/test`)
     .then(response => response.json())
     .then(parsedResponse => console.log(parsedResponse));
+
+
+    let x = document.getElementById("rock-button")
+    x.addEventListener("onclick", () => {
+        console.log("clicked")
+    })
